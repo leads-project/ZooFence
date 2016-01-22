374f30e73b252815a91ec96aed5bf4d79363fb0b
Valerio Schiavoni <valerio.schiavoni@gmail.com>
2016-01-22 17:51:09 +0100

    Builder and docker-compose for zoofence, plus DockerTest

---
+++ src/test/java/ch/unine/zoofence/DockerTest.java
@@ -0,0 +1,52 @@
+package ch.unine.zoofence;
+
+import org.apache.zookeeper.*;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Test
+public class DockerTest {
+
+    public void dockertest() throws IOException {
+        Watcher watcher = new Watcher(){
+            @Override
+            public void process(WatchedEvent event) {
+            }
+        };
+
+        List<String> connectStrings = new ArrayList<String>();
+        connectStrings.add("zookeeper0:2181");
+        connectStrings.add("zookeeper1:2182");
+
+        ZooKeeperPartitioned zkp = new ZooKeeperPartitioned(connectStrings, 1000, watcher);
+
+        try {
+            zkp.create("/node0", new byte[0], ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+            zkp.create("/node0/node1", new byte[0], ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+            zkp.create("/node0/node2", new byte[0], ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+            zkp.create("/node0/node3", new byte[0], ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+            zkp.create("/node0/node4", new byte[0], ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+        } catch (IllegalArgumentException | KeeperException| InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+        try {
+            ZKUtil.deleteRecursive(zkp, "/node0");
+        } catch (InterruptedException | KeeperException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        try {
+            zkp.close();
+        } catch (InterruptedException e) {
+            e.printStackTrace();  // TODO: Customise this generated block
+        }
+
+    }
+
+}
